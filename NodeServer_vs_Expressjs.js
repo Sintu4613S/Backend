9ee156d9b085d7d1eeae080ffe0a2bdb1e188a03
Sintu Kumar <shiva49767@gmail.com>
2026-08-30 20:23:27 +0530

    Create a file  differntiate between node vs express

---
+++ NodeServer_vs_Expressjs.js
@@ -0,0 +1,53 @@
+/*
+import express from 'express';
+
+const server = express();
+
+server.get('',(req,res)=>{
+ res.send([
+   {
+     name:'Sintu Kumar',
+     age:21
+   },
+    {
+     name:'Rakesh Kumar',
+     age:19
+   },
+    {
+     name:'Mohit Kumar',
+     age:20
+   }
+ ])
+})
+
+server.listen(8000,()=>{
+ console.log("Express js running on http://localhost:8000");
+})
+*/
+
+//Create a server using Node.js
+/*
+import http from 'http';
+
+const app = http.createServer((req, res) => {
+
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Hello from the node server");
+  }
+  else if (req.url === '/about' && req.method === '/GET') {
+    req.writeHead(200, { 'Content-Type': 'text/plain' });
+    req.end("This a about Page");
+  }
+
+  else {
+    req.writeHead(404, { 'Content-Type': 'text/plain' });
+    req.end("Page not found")
+
+  }
+})
+
+app.listen(3000, () => {
+  console.log("Node js server running on http://localhost:3000");
+});
+*/
